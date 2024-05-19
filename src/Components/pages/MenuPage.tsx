1ddc31a5d0169170ae278861b19bc033e15250d6
Dmitriy Smyshliaev <dimledlax@gmail.com>
2024-05-19 18:20:55 -0400

    Sun May 19 6:20

---
+++ src/Components/pages/MenuPage.tsx
@@ -0,0 +1,13 @@
+import Header from "../sections/HeaderSection";
+import Footer from "../sections/FooterSection";
+import HighlightsSection from "../sections/HighlightsSection";
+
+export default function HomePage() {
+  return (
+    <>
+      <Header />
+      <HighlightsSection />
+      <Footer />
+    </>
+  )
+}
